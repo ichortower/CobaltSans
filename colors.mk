91f8a68b5e23ce11e7786fd6ec8f339992b19864
ichortower <ichor@ichortower.com>
2025-11-21 12:42:03 -0500

    Add (what I think is) correct bold/colored support for all 7 ui mods.

--- colors.mk
+++ colors.mk
@@ -1,11 +1,22 @@
-COLORED_VANILLA= -fill white -opaque "\#56160c" \
-	-fill "\#4545454d" -opaque "\#e09650" \
-	-fill "\#0000004d" -opaque "\#c17c3c"
+COLORED_VANILLA= -fill "\#ffffff" -opaque "\#56160c" \
+	-fill "\#0000004d" -opaque "\#c17c3c" \
+	-fill "\#4545454d" -opaque "\#e09650"
 
-BOLD_EARTHY= -fill "\#3f2325" -opaque "\#56160c" \
+# "misc" is, in order (each one darkest to lightest):
+# arrows (2)
+# coin (3)
+# heart (3)
+# star (3)
+# guy is made of the other colors
+
+
+# earthy doesn't replace font_colored at all
+
+BOLDTEXT_EARTHY= -fill "\#3f2325" -opaque "\#56160c" \
 	-fill "\#987365" -opaque "\#c17c3c" \
-	-fill "\#b38c7c" -opaque "\#e09650" \
-	-fill "\#935f5d" -opaque "\#bf5231" \
+	-fill "\#b38c7c" -opaque "\#e09650"
+COLOREDTEXT_EARTHY=
+MISC_EARTHY= -fill "\#935f5d" -opaque "\#bf5231" \
 	-fill "\#a9756b" -opaque "\#db7439" \
 	-fill "\#ed9f06" -opaque "\#ffae00" \
 	-fill "\#efd907" -opaque "\#ffe800" \
@@ -16,11 +27,16 @@ BOLD_EARTHY= -fill "\#3f2325" -opaque "\#56160c" \
 	-fill "\#321741" -opaque "\#300068" \
 	-fill "\#7f6bd8" -opaque "\#8a60f2" \
 	-fill "\#a5ace5" -opaque "\#a0a3f2"
+BOLD_EARTHY:= ${BOLDTEXT_EARTHY} ${MISC_EARTHY}
 
-BOLD_LOWKEY= -fill "\#6a4432" -opaque "\#56160c" \
-	-fill "\#7947317a" -opaque "\#c17c3c" \
-	-fill "\#975a3e4d" -opaque "\#e09650" \
-	-fill "\#b46e4e" -opaque "\#bf5231" \
+
+BOLDTEXT_LOWKEY= -fill "\#6a4432" -opaque "\#56160c" \
+	-fill "\#975a3e92" -opaque "\#c17c3c" \
+	-fill "\#975a3e4d" -opaque "\#e09650"
+COLOREDTEXT_LOWKEY= -fill "\#e8c5b5" -opaque "\#ffffff" \
+	-fill "\#975a3e80" -opaque "\#0000004d" \
+	-fill "\#975a3e4d" -opaque "\#4545454d"
+MISC_LOWKEY= -fill "\#b46e4e" -opaque "\#bf5231" \
 	-fill "\#d2977c" -opaque "\#db7439" \
 	-fill "\#e2a256" -opaque "\#ffae00" \
 	-fill "\#ffca8a" -opaque "\#ffe800" \
@@ -31,11 +47,17 @@ BOLD_LOWKEY= -fill "\#6a4432" -opaque "\#56160c" \
 	-fill "\#af6c9f" -opaque "\#300068" \
 	-fill "\#cc9ac2" -opaque "\#8a60f2" \
 	-fill "\#e9e9fc" -opaque "\#a0a3f2"
+BOLD_LOWKEY:= ${BOLDTEXT_LOWKEY} ${MISC_LOWKEY}
+COLORED_LOWKEY:= ${COLOREDTEXT_LOWKEY} ${MISC_LOWKEY}
 
-BOLD_LAVENDER= -fill "\#583535" -opaque "\#56160c" \
+
+BOLDTEXT_LAVENDER= -fill "\#583535" -opaque "\#56160c" \
 	-fill "\#98726d" -opaque "\#c17c3c" \
-	-fill "\#c39a90" -opaque "\#e09650" \
-	-fill "\#825a5a" -opaque "\#bf5231" \
+	-fill "\#c39a90" -opaque "\#e09650"
+COLOREDTEXT_LAVENDER= -fill "\#dec4c1" -opaque "\#ffffff" \
+	-fill "\#391f1f4d" -opaque "\#0000004d" \
+	-fill "\#6944446f" -opaque "\#4545454d"
+MISC_LAVENDER= -fill "\#825a5a" -opaque "\#bf5231" \
 	-fill "\#926767" -opaque "\#db7439" \
 	-fill "\#edb558" -opaque "\#ffae00" \
 	-fill "\#f6d872" -opaque "\#ffe800" \
@@ -46,11 +68,17 @@ BOLD_LAVENDER= -fill "\#583535" -opaque "\#56160c" \
 	-fill "\#6d3a6a" -opaque "\#300068" \
 	-fill "\#b27ea8" -opaque "\#8a60f2" \
 	-fill "\#c898c0" -opaque "\#a0a3f2"
+BOLD_LAVENDER:= ${BOLDTEXT_LAVENDER} ${MISC_LAVENDER}
+COLORED_LAVENDER:= ${COLOREDTEXT_LAVENDER} ${MISC_LAVENDER}
 
-BOLD_CATVALLEY= -fill "\#5a423c" -opaque "\#56160c" \
+
+BOLDTEXT_CATVALLEY= -fill "\#5a423c" -opaque "\#56160c" \
 	-fill "\#a0887f" -opaque "\#c17c3c" \
-	-fill "\#decec4" -opaque "\#e09650" \
-	-fill "\#f58d97" -opaque "\#bf5231" \
+	-fill "\#decec4" -opaque "\#e09650"
+COLOREDTEXT_CATVALLEY= -fill "\#fdf4e9" -opaque "\#ffffff" \
+	-fill "\#d9823480" -opaque "\#0000004d" \
+	-fill "\#d982344c" -opaque "\#4545454d"
+MISC_CATVALLEY= -fill "\#f58d97" -opaque "\#bf5231" \
 	-fill "\#ffbabc" -opaque "\#db7439" \
 	-fill "\#f2a35f" -opaque "\#ffae00" \
 	-fill "\#ffe791" -opaque "\#ffe800" \
@@ -61,11 +89,17 @@ BOLD_CATVALLEY= -fill "\#5a423c" -opaque "\#56160c" \
 	-fill "\#7759a2" -opaque "\#300068" \
 	-fill "\#996cb8" -opaque "\#8a60f2" \
 	-fill "\#ada0e6" -opaque "\#a0a3f2"
+BOLD_CATVALLEY:= ${BOLDTEXT_CATVALLEY} ${MISC_CATVALLEY}
+COLORED_CATVALLEY:= ${COLOREDTEXT_CATVALLEY} ${MISC_CATVALLEY}
 
-BOLD_MAGICAL= -fill "\#68382c" -opaque "\#56160c" \
+
+BOLDTEXT_MAGICAL= -fill "\#68382c" -opaque "\#56160c" \
 	-fill "\#9a4541a0" -opaque "\#c17c3c" \
-	-fill "\#b948504c" -opaque "\#e09650" \
-	-fill "\#f58d97" -opaque "\#bf5231" \
+	-fill "\#b948504c" -opaque "\#e09650"
+COLOREDTEXT_MAGICAL= -fill "\#fcf4f0" -opaque "\#ffffff" \
+	-fill "\#d9344680" -opaque "\#0000004d" \
+	-fill "\#d934464c" -opaque "\#4545454d"
+MISC_MAGICAL= -fill "\#f58d97" -opaque "\#bf5231" \
 	-fill "\#ffbabc" -opaque "\#db7439" \
 	-fill "\#f2a35f" -opaque "\#ffae00" \
 	-fill "\#ffe791" -opaque "\#ffe800" \
@@ -76,11 +110,17 @@ BOLD_MAGICAL= -fill "\#68382c" -opaque "\#56160c" \
 	-fill "\#7759a2" -opaque "\#300068" \
 	-fill "\#996cb8" -opaque "\#8a60f2" \
 	-fill "\#ada0e6" -opaque "\#a0a3f2"
+BOLD_MAGICAL:= ${BOLDTEXT_MAGICAL} ${MISC_MAGICAL}
+COLORED_MAGICAL:= ${COLOREDTEXT_MAGICAL} ${MISC_MAGICAL}
 
-BOLD_OVERGROWN= -fill "\#684847" -opaque "\#56160c" \
+
+# overgrown and bog's forest don't tint the text color in font_colored
+
+BOLDTEXT_OVERGROWN= -fill "\#684847" -opaque "\#56160c" \
 	-fill "\#847170b4" -opaque "\#c17c3c" \
-	-fill "\#96969633" -opaque "\#e09650" \
-	-fill "\#c6b6ac" -opaque "\#bf5231" \
+	-fill "\#96969633" -opaque "\#e09650"
+COLOREDTEXT_OVERGROWN=
+MISC_OVERGROWN= -fill "\#c6b6ac" -opaque "\#bf5231" \
 	-fill "\#e3d5cb" -opaque "\#db7439" \
 	-fill "\#f8dd6f" -opaque "\#ffae00" \
 	-fill "\#f8f26f" -opaque "\#ffe800" \
@@ -91,12 +131,16 @@ BOLD_OVERGROWN= -fill "\#684847" -opaque "\#56160c" \
 	-fill "\#6e4c82" -opaque "\#300068" \
 	-fill "\#d6bcf9" -opaque "\#8a60f2" \
 	-fill "\#e9e9fc" -opaque "\#a0a3f2"
+BOLD_OVERGROWN:= ${BOLDTEXT_OVERGROWN} ${MISC_OVERGROWN}
+COLORED_OVERGROWN:= ${COLOREDTEXT_OVERGROWN} ${MISC_OVERGROWN}
 
-# coin colors omitted because no change from vanilla
-BOLD_BOGSFOREST= -fill "\#1e2e1e" -opaque "\#56160c" \
+
+BOLDTEXT_BOGSFOREST= -fill "\#1e2e1e" -opaque "\#56160c" \
 	-fill "\#2f4730" -opaque "\#c17c3c" \
-	-fill "\#14151078" -opaque "\#e09650" \
-	-fill "\#572ea3" -opaque "\#bf5231" \
+	-fill "\#14151078" -opaque "\#e09650"
+COLOREDTEXT_BOGSFOREST=
+# coin colors omitted because no change from vanilla
+MISC_BOGSFOREST= -fill "\#572ea3" -opaque "\#bf5231" \
 	-fill "\#856fc1" -opaque "\#db7439" \
 	-fill "\#5e2e3f" -opaque "\#892665" \
 	-fill "\#ac4368" -opaque "\#ff476f" \
@@ -104,3 +148,5 @@ BOLD_BOGSFOREST= -fill "\#1e2e1e" -opaque "\#56160c" \
 	-fill "\#1b2524" -opaque "\#300068" \
 	-fill "\#6fb370" -opaque "\#8a60f2" \
 	-fill "\#adf0af" -opaque "\#a0a3f2"
+BOLD_BOGSFOREST:= ${BOLDTEXT_BOGSFOREST} ${MISC_BOGSFOREST}
+COLORED_BOGSFOREST:= ${COLOREDTEXT_BOGSFOREST} ${MISC_BOGSFOREST}
--- recolor-rules.mk
+++ recolor-rules.mk
@@ -1,5 +1,6 @@
 recolors: earthy lowkey lavender catvalley magical overgrown bogsforest
 
+
 earthy: assets/recolor/font_bold_EarthyInterface.png \
 	assets/recolor/font_bold_alt_EarthyInterface.png \
 	assets/recolor/font_bold_extra_EarthyInterface.png
@@ -7,44 +8,85 @@ earthy: assets/recolor/font_bold_EarthyInterface.png \
 assets/recolor/%_EarthyInterface.png: assets/%.png
 	convert "$^" -channel RGBA ${BOLD_EARTHY} PNG32:"$@"
 
+
 lowkey: assets/recolor/font_bold_LowkeySpring.png \
 	assets/recolor/font_bold_alt_LowkeySpring.png \
-	assets/recolor/font_bold_extra_LowkeySpring.png
+	assets/recolor/font_bold_extra_LowkeySpring.png \
+	assets/recolor/font_colored_LowkeySpring.png \
+	assets/recolor/font_colored_alt_LowkeySpring.png \
+	assets/recolor/font_colored_extra_LowkeySpring.png
 
-assets/recolor/%_LowkeySpring.png: assets/%.png
+assets/recolor/font_b%_LowkeySpring.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_LOWKEY} PNG32:"$@"
+
+assets/recolor/font_c%_LowkeySpring.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_LOWKEY} PNG32:"$@"
+
 
 lavender: assets/recolor/font_bold_LavenderDreams.png \
 	assets/recolor/font_bold_alt_LavenderDreams.png \
-	assets/recolor/font_bold_extra_LavenderDreams.png
+	assets/recolor/font_bold_extra_LavenderDreams.png \
+	assets/recolor/font_colored_LavenderDreams.png \
+	assets/recolor/font_colored_alt_LavenderDreams.png \
+	assets/recolor/font_colored_extra_LavenderDreams.png
 
-assets/recolor/%_LavenderDreams.png: assets/%.png
+assets/recolor/font_b%_LavenderDreams.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_LAVENDER} PNG32:"$@"
+
+assets/recolor/font_c%_LavenderDreams.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_LAVENDER} PNG32:"$@"
+
 
 catvalley: assets/recolor/font_bold_CatValley.png \
 	assets/recolor/font_bold_alt_CatValley.png \
-	assets/recolor/font_bold_extra_CatValley.png
+	assets/recolor/font_bold_extra_CatValley.png \
+	assets/recolor/font_colored_CatValley.png \
+	assets/recolor/font_colored_alt_CatValley.png \
+	assets/recolor/font_colored_extra_CatValley.png
 
-assets/recolor/%_CatValley.png: assets/%.png
+assets/recolor/font_b%_CatValley.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_CATVALLEY} PNG32:"$@"
+
+assets/recolor/font_c%_CatValley.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_CATVALLEY} PNG32:"$@"
+
 
 magical: assets/recolor/font_bold_MagicalValley.png \
 	assets/recolor/font_bold_alt_MagicalValley.png \
-	assets/recolor/font_bold_extra_MagicalValley.png
+	assets/recolor/font_bold_extra_MagicalValley.png \
+	assets/recolor/font_colored_MagicalValley.png \
+	assets/recolor/font_colored_alt_MagicalValley.png \
+	assets/recolor/font_colored_extra_MagicalValley.png
 
-assets/recolor/%_MagicalValley.png: assets/%.png
+assets/recolor/font_b%_MagicalValley.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_MAGICAL} PNG32:"$@"
+
+assets/recolor/font_c%_MagicalValley.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_MAGICAL} PNG32:"$@"
+
+
+# alt and extra omitted from these two since not needed
 
 overgrown: assets/recolor/font_bold_OvergrownFlowery.png \
 	assets/recolor/font_bold_alt_OvergrownFlowery.png \
-	assets/recolor/font_bold_extra_OvergrownFlowery.png
+	assets/recolor/font_bold_extra_OvergrownFlowery.png \
+	assets/recolor/font_colored_OvergrownFlowery.png
 
-assets/recolor/%_OvergrownFlowery.png: assets/%.png
+assets/recolor/font_b%_OvergrownFlowery.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_OVERGROWN} PNG32:"$@"
+
+assets/recolor/font_c%_OvergrownFlowery.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_OVERGROWN} PNG32:"$@"
+
 
 bogsforest: assets/recolor/font_bold_BogsForest.png \
 	assets/recolor/font_bold_alt_BogsForest.png \
-	assets/recolor/font_bold_extra_BogsForest.png
+	assets/recolor/font_bold_extra_BogsForest.png \
+	assets/recolor/font_colored_BogsForest.png
 
-assets/recolor/%_BogsForest.png: assets/%.png
+assets/recolor/font_b%_BogsForest.png: assets/font_b%.png
 	convert "$^" -channel RGBA ${BOLD_BOGSFOREST} PNG32:"$@"
+
+assets/recolor/font_c%_BogsForest.png: assets/font_c%.png
+	convert "$^" -channel RGBA ${COLORED_BOGSFOREST} PNG32:"$@"
+
